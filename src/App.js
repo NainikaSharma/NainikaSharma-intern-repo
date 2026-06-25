47c3968004fc1dbef16de84d268bb8e6ea5e1899
NainikaSharma <117586002+NainikaSharma@users.noreply.github.com>
2026-06-25 18:04:04 +1000

    Add React Router navigation with Home and Profile pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
+import Home from "./Home";
+import Profile from "./Profile";
 import Login from "./Login";
 import UserForm from "./UserForm";
 import Parent from "./Parent";
@@ -7,26 +10,34 @@ import MemoExample from "./MemoExample";
 import UseEffectDemo from "./UseEffectDemo";
 
 function App() {
-  const [page, setPage] = useState("login");
-
   return (
-    <div style={{ padding: "20px" }}>
-      {/* Navigation */}
-      <div style={{ marginBottom: "20px" }}>
-        <button onClick={() => setPage("login")}>Login</button>
-        <button onClick={() => setPage("form")}>Form</button>
-        <button onClick={() => setPage("callback")}>useCallback</button>
-        <button onClick={() => setPage("memo")}>useMemo</button>
-        <button onClick={() => setPage("effect")}>useEffect</button>
-      </div>
+    <Router>
+      <div style={{ padding: "20px" }}>
 
-      {/* Pages */}
-      {page === "login" && <Login />}
-      {page === "form" && <UserForm />}
-      {page === "callback" && <Parent />}
-      {page === "memo" && <MemoExample />}
-      {page === "effect" && <UseEffectDemo />}
-    </div>
+        {/* Navigation */}
+        <nav style={{ marginBottom: "20px" }}>
+          <Link to="/" style={{ marginRight: "10px" }}>Login</Link>
+          <Link to="/home" style={{ marginRight: "10px" }}>Home</Link>
+          <Link to="/profile" style={{ marginRight: "10px" }}>Profile</Link>
+          <Link to="/form" style={{ marginRight: "10px" }}>Form</Link>
+          <Link to="/memo" style={{ marginRight: "10px" }}>useMemo</Link>
+          <Link to="/callback" style={{ marginRight: "10px" }}>useCallback</Link>
+          <Link to="/effect">useEffect</Link>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/form" element={<UserForm />} />
+          <Route path="/memo" element={<MemoExample />} />
+          <Route path="/callback" element={<Parent />} />
+          <Route path="/effect" element={<UseEffectDemo />} />
+        </Routes>
+
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+
+      <button onClick={() => navigate("/profile")}>
+        Go to Profile
+      </button>
+    </div>
+  );
+}
+
+export default Home;
